5b22068903ad378075a72cc5397b28fd4bcab876
PeteChatge <66chatgepete@gmail.com>
2026-02-21 12:47:49 +0100

    test-NAchricht!

--- gacp.bat
+++ gacp.bat
@@ -1,9 +1,16 @@
 @echo off
+cd /d C:\Users\%username%\Downloads\GITHUB\videotools
+
 if "%~1"=="" (
-    echo Bitte eine Commit-Nachricht angeben!
-    echo Beispiel: gacp "Meine Nachricht"
-    exit /b 1
+    set /p message="Commit-Nachricht eingeben: "
+) else (
+    set message=%*
 )
+
 git add .
-git commit -m %*
+git commit -m "%message%"
 git push
+
+echo.
+echo Fertig! Druecke eine Taste zum Beenden...
+pause >nul
